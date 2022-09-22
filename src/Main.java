35fbf540a451a0ffa4a853d1b007c1b2f4727228
Sboti03 <somogyi.botond.munka@gmail.com>
2022-09-22 08:46:25 +0200

    2. feladat a, b

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Festmeny> festmenyek = new ArrayList<>();
+        festmenyek.add(new Festmeny("Hollókő", "Bálint Ferenc", "Expresszionizmus"));
+        festmenyek.add(new Festmeny("Hómező", "Bálint Ferenc", "Konstruktivizmus"));
+        festmenyek.add(new Festmeny("Reggel", "Bálint Ferenc", "Futurizmus"));
+
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Kérem adjon meg egy darabszámot");
+        int darabSzam = sc.nextInt();
+        for (int i = 0; i < darabSzam; i++) {
+            System.out.println("Kérem adja meg a festmény címét: ");
+            String cim = sc.nextLine();
+            System.out.println("Kérem adja meg a festő nevét:");
+            String festo = sc.nextLine();
+            System.out.println("Kérem adja meg a stílist:");
+            String stilus = sc.nextLine();
+            festmenyek.add(new Festmeny(cim,festo,stilus));
+        }
     }
 }
